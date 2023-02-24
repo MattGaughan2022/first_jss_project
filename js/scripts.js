b18dcb2db8e8e12ff2b8997d60cf9e7f14009e38
Matt Gaughan <mattgaugs@gmail.com>
2023-02-24 13:52:16 -0500

    added pokedex starters

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,17 @@
-alert('Hello world');
+//backslash in a string to announce non-normal reading of a symbol
+// like "she said \"Yes!\" to the proposal"
+// complex vs primitive data types
+// complex uses pointers to stored data vs primitive
+// containing data directly
 
-let favoriteFood = 'tacos';
-document.write(favoriteFood);
+
+let pokemonList = [
+    { name: 'Bulbasaur', height: .7, types: ['grass', 'poison']},
+    { name: 'Charmander', height: .6, types: ['fire']},
+    { name: 'Squirtle', height: .5, types: ['water']},
+    { name: 'Pikachu', height: .4, types: ['electric']}
+];
+console.log(pokemonList[0]);
+console.log(pokemonList[1]);
+console.log(pokemonList[2]);
+console.log(pokemonList[3]);
