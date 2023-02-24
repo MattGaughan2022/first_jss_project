5772672f3ebb28bcfdeb6c3109d20b9f471120d2
Matt Gaughan <mattgaugs@gmail.com>
2023-02-24 14:50:00 -0500

    modified array layout, added loop to print array

--- js/scripts.js
+++ js/scripts.js
@@ -6,12 +6,32 @@
 
 
 let pokemonList = [
-    { name: 'Bulbasaur', height: .7, types: ['grass', 'poison']},
-    { name: 'Charmander', height: .6, types: ['fire']},
-    { name: 'Squirtle', height: .5, types: ['water']},
-    { name: 'Pikachu', height: .4, types: ['electric']}
+    { 
+        name: 'Bulbasaur', 
+        height: .7, 
+        types: ['grass', ' poison']},
+    { 
+        name: 'Charmander', 
+        height: .6, 
+        types: ['fire']},
+    { 
+        name: 'Squirtle', 
+        height: .5, 
+        types: ['water']},
+    { 
+        name: 'Pikachu', 
+        height: .4, 
+        types: ['electric']}
 ];
-console.log(pokemonList[0]);
-console.log(pokemonList[1]);
-console.log(pokemonList[2]);
-console.log(pokemonList[3]);
+
+for(let i = 0; i<pokemonList.length; i++){
+    document.write(`<br>${pokemonList[i].name} (${pokemonList[i].height}m) type: ${pokemonList[i].types}`);
+    if(pokemonList[i].height < .5){
+        document.write(' <--- What a small pokemon!');
+    }
+}
+
+// console.log(pokemonList[0]);
+// console.log(pokemonList[1]);
+// console.log(pokemonList[2]);
+// console.log(pokemonList[3]);
